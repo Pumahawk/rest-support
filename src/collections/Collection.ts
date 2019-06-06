6502afec60581e36c91e1ede6265be49ea1d4ba1
Lorenzo Gandino <lorenzoga94@gmail.com>
2019-06-06 23:59:48 +0200

    Ricostruita la libreria@

--- src/collections/Collection.ts
+++ src/collections/Collection.ts
@@ -2,26 +2,31 @@ import { Context, RestResponse } from "../support";
 import { Document } from "../documents/Document";
 const axios = require('axios');
 
-export abstract class Collection<T extends Document, P = string> {
+interface AxiosRes<D> {
+    data: D;
+}
 
-    public abstract url(context: Context, param: P): string;
-    public abstract documentGenerator(data: any): T;
+export abstract class Collection<DReq, T extends Document<DReq, DRes>, Data = any, P = any, DRes = DReq> {
 
-    public get(param: P): Promise<RestResponse<T>> {
+    public abstract url(context: Context, param: P | null): string;
+    public abstract extract(data: DRes): T;
+    public abstract serialize<D>(data: D): DReq;
+
+    public async get(param: P): Promise<RestResponse<T>> {
         const context = new Context();
         context.method = 'GET';
         const collection = this;
-        return axios.get(this.url(context, param)).then((resp: any) => {
-            return new RestResponse(collection.documentGenerator(resp.data));
+        return axios.get(this.url(context, param)).then((resp: AxiosRes<DRes>) => {
+            return new RestResponse(collection.extract(resp.data));
         });
     }
 
-    public add(document: T): Promise<RestResponse<T>> {
+    public async create(document: Data): Promise<RestResponse<T>> {
         const context = new Context();
         context.method = 'POST';
         const collection = this;
-        return axios.post(this.url(context, param)).then((resp: any) => {
-            return new RestResponse(collection.documentGenerator(resp.data));
+        return axios.post(this.url(context, null), this.serialize(document)).then((resp: AxiosRes<DRes>) => {
+            return new RestResponse(collection.extract(resp.data));
         });
     }
 }
--- src/documents/Document.ts
+++ src/documents/Document.ts
@@ -1,46 +1,41 @@
 import { Context, ActionReport, RestResponse } from "../support";
 const axios = require('axios');
 
-export abstract class Document<DtoResponse = any, Data = DtoResponse, DtoRequest = DtoResponse> {
+export abstract class Document<DReq, DRes = DReq> {
 
-    public data: Data;
-
-    constructor(data: Data) {
-        this.data = data;
-    }
     public abstract url(context: Context): string;
-    public abstract extractor(dto: DtoResponse): Data;
-    public abstract serializer(data: Data): DtoRequest;
+    public abstract extractor(dto: DRes): void;
+    public abstract serializer(): DReq;
 
-    public async delete(): Promise<RestResponse<Document<DtoResponse, Data>>> {
+    public async delete(): Promise<RestResponse<Document<DReq, DReq>>> {
         const context = new Context;
         context.method = "DELETE";
         const url = this.url(context);
         const document = this;
         return axios.delete(url).then((resp: any) => {
-            document.data = this.extractor(resp.data);
+            this.extractor(resp.data);
             return new RestResponse(document);
         });
     }
 
-    public async commit(): Promise<RestResponse<Document<DtoResponse, Data>>> {
+    public async commit(): Promise<RestResponse<Document<DReq, DReq>>> {
         const context = new Context;
         context.method = "PUT";
         const url = this.url(context);
         const document = this;
-        return axios.put(url).then((resp: any) => {
-            document.data = this.extractor(resp.data);
+        return axios.put(url, this.serializer()).then((resp: any) => {
+            this.extractor(resp.data);
             return new RestResponse(document)
         });
     }
 
-    public async update(): Promise<RestResponse<Document<DtoResponse, Data>>> {
+    public async update(): Promise<RestResponse<Document<DReq, DReq>>> {
         const context = new Context;
         context.method = "GET";
         const url = this.url(context);
         const document = this;
         return axios.get(url).then((resp: any) => {
-            document.data = this.extractor(resp.data);
+            this.extractor(resp.data);
             return new RestResponse(document);
         });
     }
